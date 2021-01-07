11eb7c35c96e770b3316e4204647f051726d0d30
SwingGuy1024 <SwingGuy1024@yahoo.com>
2021-01-07 00:41:49 -0800

    Initial commit

---
+++ src/test/java/com/openAPIDemo/bug/BugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openAPIDemo.bug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
